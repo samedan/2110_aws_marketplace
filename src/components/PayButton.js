c97d8385d6d7fabafd8cdeb384a34afbc2aae800
Daniel Popescu <same.dan@gmail.com>
2021-11-02 12:13:44 +0100

    Stripe Integration

--- src/components/PayButton.js
+++ src/components/PayButton.js
@@ -1,8 +1,26 @@
 import React from "react";
+import StripeCheckout from "react-stripe-checkout";
 // import { Notification, Message } from "element-react";
 
-const PayButton = () => {
-  return <div>PayButton</div>;
+const stripeConfig = {
+  currency: "USD",
+  publishableAPIKey: "pk_test_gJgNpW1KQNGR4DQGcH0v5jZO",
+};
+
+const PayButton = ({ product, user }) => {
+  return (
+    <StripeCheckout
+      email={user.attributes.email}
+      name={product.description}
+      amount={product.price}
+      currency={stripeConfig.currency}
+      stripeKey={stripeConfig.publishableAPIKey}
+      shippingAddress={product.shipped}
+      billingAddress={product.shippingAddress}
+      locale="auto"
+      allowRememberMe="false"
+    />
+  );
 };
 
 export default PayButton;
--- src/components/Product.js
+++ src/components/Product.js
@@ -106,7 +106,9 @@ class Product extends React.Component {
                     <span className="mx-1">
                       ${convertCentsToDollars(product.price)}
                     </span>
-                    {!isProductOwner && <PayButton />}
+                    {!isProductOwner && (
+                      <PayButton product={product} user={user} />
+                    )}
                   </div>
                 </div>
               </Card>
